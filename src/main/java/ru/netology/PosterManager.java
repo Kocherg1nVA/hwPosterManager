dd0aa820a88e8114e6b2715d39c57e19b210db11
Vadim Kochergin <kochergin1990vadim@gmail.com>
2024-12-15 11:10:00 +0300

    findRequiredLength method added for advanced findAll method

--- src/main/java/ru/netology/PosterManager.java
+++ src/main/java/ru/netology/PosterManager.java
@@ -1,45 +1,59 @@
 package ru.netology;
 
-public class PosterManager{
+public class PosterManager {
 
-   private String[] movie = new String[0];
-   private int limit;
-
-
-   public PosterManager(){
-      this.limit = 5;
-   }
-
-   public PosterManager(int limit){
-      this.limit = limit;
-   }
-
-   public void addMovie(String movie){
-      String[] tmp = new String[this.movie.length + 1];
-      for (int i = 0; i < this.movie.length; i++){
-         tmp[i] = this.movie[i];
-      }
-      tmp[tmp.length - 1] = movie;
-      this.movie = tmp;
-   }
-
-   public String[] findAll (){
-      return this.movie;
-   }
-
-   public String[] findLast(){
-      int requiredLength;
-      if (this.movie.length < limit){
-         requiredLength = this.movie.length;
-      } else requiredLength = this.limit;
-      String[] last = new String[requiredLength];
-      for (int i = 0; i < this.movie.length; i++) {
-         last[i] = this.movie[this.movie.length - 1 - i];
-      }
-      return last;
-   }
+    private String[] movie = new String[0];
+    private int limit;
+    private int requiredLength;
 
 
+    public PosterManager() {
+        this.limit = 5;
+    }
 
+    public PosterManager(int limit) {
+        this.limit = limit;
+    }
 
+    public void addMovie(String movie) {
+        String[] tmp = new String[this.movie.length + 1];
+        for (int i = 0; i < this.movie.length; i++) {
+            tmp[i] = this.movie[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        this.movie = tmp;
+    }
+
+    public String[] findAll() {
+        return this.movie;
+    }
+
+    public void findRequirementLength() {
+        if (this.movie.length < this.limit) {
+            this.requiredLength = this.movie.length;
+        } else {
+            this.requiredLength = this.limit;
+        }
+    }
+
+    public String[] findLast() {
+
+        String[] last = new String[0];
+
+        if (this.requiredLength < this.movie.length) {
+            int result = this.movie.length - this.limit;
+            String[] tmp1 = new String[limit];
+            for (int i = 0; i < this.movie.length - result; i++) {
+                tmp1[i] = this.movie[this.movie.length - 1 - i];
+                last = tmp1;
+            }
+        } else {
+            String[] tmp2 = new String[this.requiredLength];
+            for (int i = 0; i < this.movie.length; i++) {
+                tmp2[i] = this.movie[this.movie.length - 1 - i];
+                last = tmp2;
+            }
+        }
+        return last;
+    }
 }
--- src/test/java/ru/netology/PosterManagerTest.java
+++ src/test/java/ru/netology/PosterManagerTest.java
@@ -18,11 +18,12 @@ public class PosterManagerTest {
     }
 
     @Test
-    public void shouldFindLast(){
+    public void shouldFindLastIfLimitMoreThenMoviesLength() {
         PosterManager manager = new PosterManager();
         manager.addMovie("Hellboy");
         manager.addMovie("Deadpool");
         manager.addMovie("Alien");
+        manager.findRequirementLength();
         manager.findLast();
         String[] expected = {"Alien", "Deadpool", "Hellboy"};
         String[] actual = manager.findLast();
@@ -30,15 +31,18 @@ public class PosterManagerTest {
     }
 
     @Test
-    public void shouldFindLastIfMoreThenFive(){
+    public void shouldFindLastIfLimitLowThenMoviesLength() {
         PosterManager manager = new PosterManager(6);
+        manager.addMovie("Deadpool");
         manager.addMovie("Despicable Me 4");
         manager.addMovie("Wicked");
         manager.addMovie("Moana 2");
         manager.addMovie("Beetlejuice Beetlejuice");
         manager.addMovie("Dune: Part Two");
         manager.addMovie("Twisters");
-        String[] expected = {"Twisters", "Dune: Part Two", "Beetlejuice Beetlejuice", "Moana 2", "Wicked", "Despicable Me 4"};
+        manager.findRequirementLength();
+        String[] expected = {"Twisters", "Dune: Part Two", "Beetlejuice Beetlejuice", "Moana 2", "Wicked",
+                "Despicable Me 4"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
